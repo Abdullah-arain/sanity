b7972d3eb48d35cc2c89ab8db9aeb8508eaed4bf
abdullaharain1309@gmail.com <abdullaharain1309@gmail.com>
2024-12-23 19:04:56 -0800

    used interface for type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
 import { client } from "@/sanity/lib/client";
 
+interface Item {
+  productName: string,
+  price: number,
+}
+
 export default async function Home() {
   const data = await client.fetch(`*[_type == 'product']`);
-  console.log(data);
+  // console.log(data);
+  
   return (
     <div>
       <section>
-        {data.map((item: any, i) => {
+        {data.map((item: Item, i: number) => {
           return (
-            <div className="flex gap-3">
+            <div className="flex gap-3" key={i}>
               <h1>{item.productName}</h1>
               <p>$ {item.price}</p>
             </div>
